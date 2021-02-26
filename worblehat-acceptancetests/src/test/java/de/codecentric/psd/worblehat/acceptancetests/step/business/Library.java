864d1bfba9adb8206b34717158406c555bc85ac1
Manuel Berger <manuel.berger@codecentric.de>
2021-02-26 15:32:34 +0100

    fixed sonarlint rules

--- worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/acceptancetests/step/business/Library.java
+++ worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/acceptancetests/step/business/Library.java
@@ -16,79 +16,69 @@ import org.springframework.context.ApplicationContext;
 // @Component("Library")
 public class Library {
 
-  @Autowired(required = true)
-  private BookService bookService;
+    @Autowired(required = true)
+    private BookService bookService;
 
-  @Autowired
-  public Library(ApplicationContext applicationContext) {
-    this.bookService = applicationContext.getBean(BookService.class);
-  }
-
-  // *******************
-  // *** G I V E N *****
-  // *******************
-
-  @Given("an empty library")
-  public void emptyLibrary() {
-    bookService.deleteAllBooks();
-  }
-
-  @Given("a library, containing a book with isbn {string}")
-  public void createLibraryWithSingleBookWithGivenIsbn(String isbn) {
-    Book book = DemoBookFactory.createDemoBook().withISBN(isbn).build();
-    bookService.createBook(
-        book.getTitle(), book.getAuthor(), book.getEdition(), isbn, book.getYearOfPublication());
-  }
-
-  @Given("{string} has borrowed books {string}")
-  public void borrower1HasBorrowerdBooks(String borrower, String isbns) {
-    borrowerHasBorrowedBooks(borrower, isbns);
-  }
-
-  public void borrowerHasBorrowedBooks(String borrower, String isbns) {
-    List<String> isbnList = getListOfItems(isbns);
-    for (String isbn : isbnList) {
-      Book book = DemoBookFactory.createDemoBook().withISBN(isbn).build();
-      bookService
-          .createBook(
-              book.getTitle(),
-              book.getAuthor(),
-              book.getEdition(),
-              isbn,
-              book.getYearOfPublication())
-          .orElseThrow(IllegalStateException::new);
-
-      bookService.borrowBook(book.getIsbn(), borrower);
+    @Autowired
+    public Library(ApplicationContext applicationContext) {
+        this.bookService = applicationContext.getBean(BookService.class);
     }
-  }
 
-  private List<String> getListOfItems(String isbns) {
-    return isbns.isEmpty() ? Collections.emptyList() : Arrays.asList(isbns.split(" "));
-  }
-  // *****************
-  // *** W H E N *****
-  // *****************
+    // *******************
+    // *** G I V E N *****
+    // *******************
 
-  // *****************
-  // *** T H E N *****
-  // *****************
-
-  @Then("the library contains {int} copies of the book with {string}")
-  public void shouldContainCopiesOfBook(Integer nrOfCopies, String isbn) {
-    waitForServerResponse();
-    Set<Book> books = bookService.findBooksByIsbn(isbn);
-    assertThat(books.size(), is(nrOfCopies));
-    assertThat(books, everyItem(hasProperty("isbn", is(isbn))));
-  }
-
-  private void waitForServerResponse() {
-    // normally you would have much better mechanisms for waiting for a
-    // server response. We are choosing a simple solution for the sake of this
-    // training
-    try {
-      Thread.sleep(100);
-    } catch (InterruptedException e) {
-      // pass
+    @Given("an empty library")
+    public void emptyLibrary() {
+        bookService.deleteAllBooks();
     }
-  }
+
+    @Given("a library, containing a book with isbn {string}")
+    public void createLibraryWithSingleBookWithGivenIsbn(String isbn) {
+        Book book = DemoBookFactory.createDemoBook().withISBN(isbn).build();
+        bookService.createBook(book.getTitle(), book.getAuthor(), book.getEdition(), isbn, book.getYearOfPublication());
+    }
+
+    @Given("{string} has borrowed books {string}")
+    public void borrower1HasBorrowerdBooks(String borrower, String isbns) {
+        borrowerHasBorrowedBooks(borrower, isbns);
+    }
+
+    public void borrowerHasBorrowedBooks(String borrower, String isbns) {
+        List<String> isbnList = getListOfItems(isbns);
+        for (String isbn : isbnList) {
+            Book book = DemoBookFactory.createDemoBook().withISBN(isbn).build();
+            bookService
+                    .createBook(book.getTitle(), book.getAuthor(), book.getEdition(), isbn, book.getYearOfPublication())
+                    .orElseThrow(IllegalStateException::new);
+
+            bookService.borrowBook(book.getIsbn(), borrower);
+        }
+    }
+
+    private List<String> getListOfItems(String isbns) {
+        return isbns.isEmpty() ? Collections.emptyList() : Arrays.asList(isbns.split(" "));
+    }
+    // *****************
+    // *** W H E N *****
+    // *****************
+
+    // *****************
+    // *** T H E N *****
+    // *****************
+
+    @Then("the library contains {int} copies of the book with {string}")
+    public void shouldContainCopiesOfBook(Integer nrOfCopies, String isbn) {
+        waitForServerResponse();
+        Set<Book> books = bookService.findBooksByIsbn(isbn);
+        assertThat(books.size(), is(nrOfCopies));
+        assertThat(books, everyItem(hasProperty("isbn", is(isbn))));
+    }
+
+    private void waitForServerResponse() {
+        // normally you would have much better mechanisms for waiting for a
+        // server response. We are choosing a simple solution for the sake of this
+        // training
+
+    }
 }
