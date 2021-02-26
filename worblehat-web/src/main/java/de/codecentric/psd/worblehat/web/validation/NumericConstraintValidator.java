5b4d752ea288ca14e6ebda1d0d879d4b9f31f6f7
Manuel Berger <manuel.berger@codecentric.de>
2021-02-26 15:15:40 +0100

    fixed sonarlint rules

--- worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/NumericConstraintValidator.java
+++ worblehat-web/src/main/java/de/codecentric/psd/worblehat/web/validation/NumericConstraintValidator.java
@@ -7,11 +7,9 @@ import org.apache.commons.lang3.StringUtils;
 public class NumericConstraintValidator implements ConstraintValidator<Numeric, String> {
 
   @Override
-  public void initialize(Numeric constraintAnnotation) {}
-
-  @Override
   public boolean isValid(String value, ConstraintValidatorContext context) {
-    // Don't validate null, empty and blank strings, since these are validated by @NotNull,
+    // Don't validate null, empty and blank strings, since these are validated by
+    // @NotNull,
     // @NotEmpty and @NotBlank
     if (StringUtils.isNotBlank(value)) {
       return StringUtils.isNumeric(value);
